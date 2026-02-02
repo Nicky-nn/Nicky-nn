55bec166248c0968db715027be0d396173ecfb7f
Nick <64853411+Nicky-nn@users.noreply.github.com>
2026-02-02 14:37:49 -0400

    updated

--- index.js
+++ index.js
@@ -25,47 +25,88 @@ function generateProgressBar() {
 }
 
 const readme = `
+<p align="center">
+  <img src="https://img.shields.io/badge/Birth_Year-2002-pink" alt="Birth Year" style="height: 25px; vertical-align: middle;"/>
+  <img src="https://img.shields.io/badge/Learning-Next.js%20%26%20TypeScript-yellow" alt="Learning" style="height: 25px; vertical-align: middle;"/>
+  <img src="https://img.shields.io/badge/Job_Status-Actively_Seeking-brightgreen" alt="Job Status" style="height: 25px; vertical-align: middle;"/>
+</p>
+
 <h1 align="center">Hi <img src="https://github.com/TheDudeThatCode/TheDudeThatCode/blob/master/Assets/Hi.gif" width="35" />, I'm Nick</h1>
+
+<p align="center">
+  <img src="./nick.gif" alt="Boy Programming" width="340" />
+</p>
+
+## 👦 About Me
+
 <h3 align="center">I'm Nick Russell, a ${age}-year-old Bolivian Independent FullStack Developer, a weird guy who likes to do weird things with technologies and related to design, developing experiences and web applications. When I'm not working or playing, I plan how to escape from the University.</h3>
 
-- 👨‍💻 All of my projects are available at [https://nick-russell.vercel.app/](https://nick-russell.vercel.app/) <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
+-   👨‍💻 All of my projects are available at [https://nick-russell.vercel.app/](https://nick-russell.vercel.app/) <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
 
-- 💬 Ask me about **react, vue, angular**
+-   💬 Ask me about **react, vue, angular**
 
-- 📫 How to reach me **nick077n@gmail.com**
+-   📫 How to reach me **nick077n@gmail.com**
 
-- 👯 We can connect to play some games ♟
+-   👯 We can connect to play some games ♟
 
-- ⚡ Fun fact **Je connais un peu le français**
-
-- 🧑‍💻 Tech I work on :
-
-
-<h3 align="left">Languages and Tools:</h3>
-<p align="center"> <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/> </a> <a href="https://flutter.dev" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" alt="flutter" width="40" height="40"/> </a> <a href="https://golang.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" alt="go" width="40" height="40"/> </a> <a href="https://graphql.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="graphql" width="40" height="40"/> </a> <a href="https://ionicframework.com" target="_blank" rel="noreferrer"> <img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Ionic_Logo.svg" alt="ionic" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://kotlinlang.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg" alt="kotlin" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40"/> </a> <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a> <a href="https://developer.apple.com/swift/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/swift/swift-original.svg" alt="swift" width="40" height="40"/> </a> </p>
-
+-   ⚡ Fun fact **Je connais un peu le français**
 
 ---
-### <img src='https://media1.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif?cid=ecf05e47x2g034i9pzwtzzsd3xgg2w9nr94t4tflbbgo3008&rid=giphy.gif' width='25' /> My Github Stats:
-![Nick's github stats](https://github-readme-stats.vercel.app/api?username=nicky-nn&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=issues&count_private=true&include_all_commits=true)
-[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=nicky-nn&layout=compact&text_color=daf7dc&bg_color=151515&hide=css,html,php)](https://github.com/anuraghazra/github-readme-stats)
-[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=nicky-nn&theme=dark)](https://git.io/streak-stats)
+
+## 📈 GitHub Stats
+
+<div>
+  <img align="center" src="https://github-readme-streak-stats-eight.vercel.app/?user=nicky-nn&theme=react" width="51%" />
+  <img align="center" src="https://github-readme-stats-matin.vercel.app/api?username=nicky-nn&layout=compact&show_icons=true&theme=react" alt="MatinT-SA github-readme-stats" width="48%" />
+</div>
+<br />
+<div>
+  <img align="center" src="https://github-readme-stats-matin.vercel.app/api/top-langs/?username=nicky-nn&layout=compact&theme=react" alt="MatinT-SA github readme top-langs" width="39%" />
+  <img align="center" src="https://github-readme-activity-graph.vercel.app/graph?username=nicky-nn&theme=merko" alt="MatinT-SA github readme activity graph" width="60%" />
+</div>
+
+<br />
+
+---
+
+## 🔧 Technologies & Tools
+
+<div align="center">
+
+| Frontend Technologies                                                                                                               | Backend Technologies                                                                                                                | Databases                                                                                                                                       | Tools & Libraries                                                                                                                           | Deployment Tools                                                                                                                      |
+| ----------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------- |
+| ![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white&labelColor=CD3700)                | ![ASP.NET Core](https://img.shields.io/badge/ASP.NET%20Core-512BD4?style=for-the-badge&logo=.net&logoColor=white&labelColor=3A5FCD) | ![SQL Server](https://img.shields.io/badge/SQL%20Server-CC2927?style=for-the-badge&logo=microsoft-sql-server&logoColor=white&labelColor=8B2323) | ![Bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white&labelColor=8B7355)            | ![Vercel](https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white&labelColor=8B7355)               |
+| ![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white&labelColor=104E8B)                   | ![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white&labelColor=2E8B57)          | ![MongoDB](https://img.shields.io/badge/MongoDB-47A248?style=for-the-badge&logo=mongodb&logoColor=white&labelColor=004D40)                      | ![Material-UI](https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=material-ui&logoColor=white&labelColor=104E8B)     | ![Netlify](https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white&labelColor=8B7355)            |
+| ![JavaScript](https://img.shields.io/badge/JavaScript-EDD600?style=for-the-badge&logo=javascript&logoColor=white&labelColor=C8A800) | ![MERN Stack](https://img.shields.io/badge/MERN%20Stack-00D8FF?style=for-the-badge&logo=react&logoColor=white&labelColor=8B3A3A)    | ![Supabase](https://img.shields.io/badge/Supabase-3ECF8E?style=for-the-badge&logo=supabase&logoColor=white&labelColor=006E58)                   | ![Tailwind CSS](https://img.shields.io/badge/Tailwind%20CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white&labelColor=8B8378) | ![GitHub Pages](https://img.shields.io/badge/GitHub%20Pages-222222?style=for-the-badge&logo=github&logoColor=white&labelColor=8B7355) |
+| ![React](https://img.shields.io/badge/React-20232a?style=for-the-badge&logo=react&logoColor=61DAFB&labelColor=101010)               | ![REST APIs](https://img.shields.io/badge/REST%20APIs-FF6F61?style=for-the-badge&logo=rest&logoColor=white&labelColor=8B7355)       |                                                                                                                                                 | ![WordPress](https://img.shields.io/badge/WordPress-21759B?style=for-the-badge&logo=wordpress&logoColor=white&labelColor=104E8B)            |                                                                                                                                       |
+| ![Next.js](https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white&labelColor=8B7355)          |                                                                                                                                     |                                                                                                                                                 | ![Redux](https://img.shields.io/badge/Redux-764ABC?style=for-the-badge&logo=redux&logoColor=white&labelColor=8B7355)                        |                                                                                                                                       |
+|             |                                                                                                                                     |                                                                                                                                                 | ![Framer Motion](https://img.shields.io/badge/Framer%20Motion-0055FF?style=for-the-badge&logo=framer&logoColor=white&labelColor=8B7355)     |                                                                                                                                       |
+| ![SASS](https://img.shields.io/badge/SASS-CC6699?style=for-the-badge&logo=sass&logoColor=white&labelColor=8B7355)                   |                                                                                                                                     |                                                                                                                                                 | ![Three.js](https://img.shields.io/badge/Three.js-000000?style=for-the-badge&logo=three.js&logoColor=white&labelColor=8B7355)               |                                                                                                                                       |
+|                                                                                                                                     |                                                                                                                                     |                                                                                                                                                 | ![Vite](https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=vite&logoColor=white&labelColor=8B7355)                           |                                                                                                                                       |
+
+</div>
+
+---
+
 
 <!--START_SECTION:waka-->
 
 <!--END_SECTION:waka-->
 
-
 ⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${new Date().getDate()+'-'+ monthNames[new Date().getMonth()]+'-'+new Date().getFullYear()}
 
 ---
+## 🏆 Achievements
 
-<h3 align="left">Connect with me:</h3>
-<p align="left">
-<a href="https://dev.to/nickynn" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/devto.svg" alt="nickynn" height="30" width="40" /></a>
-<a href="https://twitter.com/nick_russell_in" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="nick_russell_in" height="30" width="40" /></a>
-<a href="https://linkedin.com/in/nickynn/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="nickynn/" height="30" width="40" /></a>
-</p>
+![trophy](https://github-profile-trophy-tawny.vercel.app/?username=nicky-nn&theme=onedark&margin-w=15&no-frame=true)
 
+---
+## 📫 Connect with Me
+
+[![Twitter](https://img.shields.io/badge/Follow%20me-%23000000?style=for-the-badge&logo=X&logoColor=white)](https://x.com/Nick_Russell077)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5?style=for-the-badge&logo=LinkedIn&logoColor=white)](https://www.linkedin.com/in/nickynn/)
+[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F?style=for-the-badge&logo=Instagram&logoColor=white)](https://www.instagram.com/nick_russell07/)
+[![Gmail](https://img.shields.io/badge/Gmail-%23EA4335?style=for-the-badge&logo=Gmail&logoColor=white)](mailto:nick077n@gmail.com)
+[![dev.to](https://img.shields.io/badge/dev.to-%23000000?style=for-the-badge&logo=dev.to&logoColor=white)](https://dev.to/nickynn)
 `
 console.log(readme)
